e52309d5b22a5f4a091d862b61f82e16baed4f23
Tamim Ahmed <ahmed241-16-034@diu.edu.bd>
2024-07-16 22:35:33 +0600

    Add files via upload

---
+++ 2D.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const petals = document.querySelectorAll('.petal');
+    petals.forEach((petal, index) => {
+        petal.style.animationDelay = `${index * 0.5}s`;
+    });
+});
+
